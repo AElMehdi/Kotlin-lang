d48a163bab8b706186ac728572295475c23a132a
AElMehdi <assirelmehdi@gmail.com>
2020-05-21 23:52:41 +0200

    Koans, introduction: Named and default params

--- src/main/kotlin/com/aelmehdi/koans/Introduction.kt
+++ src/main/kotlin/com/aelmehdi/koans/Introduction.kt
@@ -3,8 +3,18 @@ package com.aelmehdi.koans
 class Introduction
 
 fun main(args: Array<String>) {
-    print(start())
+    println(start())
+    println(joinOptions(mutableListOf("option 1", "option 2", "option 3", "option 4")))
 }
 
 
+// Hello World
 fun start(): String = "OK"
+
+// Named and default parameters
+fun joinToString(seperator: String = ",", prefix: String = "", postfix: String = ""): String {
+    return "I've been called"
+}
+
+fun joinOptions(options: Collection<String>): String = joinToString(prefix = "[", postfix = "]")
+
