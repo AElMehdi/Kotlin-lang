362606c0c8c4781312180fbb1edbcad37e690690
AElMehdi <assirelmehdi@gmail.com>
2020-05-22 18:12:42 +0200

    Koans, introduction: Lambdas

--- src/main/kotlin/com/aelmehdi/koans/Introduction.kt
+++ src/main/kotlin/com/aelmehdi/koans/Introduction.kt
@@ -5,6 +5,7 @@ class Introduction
 fun main(args: Array<String>) {
     println(start())
     println(joinOptions(mutableListOf("option 1", "option 2", "option 3", "option 4")))
+    println(containsEven(mutableListOf(3, 1, 5, 7)))
 }
 
 
@@ -18,3 +19,6 @@ fun joinToString(seperator: String = ",", prefix: String = "", postfix: String =
 
 fun joinOptions(options: Collection<String>): String = joinToString(prefix = "[", postfix = "]")
 
+
+// Lambdas
+fun containsEven(numbers: Collection<Int>) = numbers.any { it % 2 == 0 }
